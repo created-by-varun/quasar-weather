4162ae884eb0749493ed2a77397fa63328dbb0ed
Varun V <varunvirupaksha321@gmail.com>
2020-05-27 22:47:12 +0530

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-page class="flex column">
+  
+  <div class="col q-pt-lg q-px-md">
+  	<q-input
+  	v-model="search"
+  	placeholder="Search Location"
+  	dark
+  	borderless
+  	>
+  	
+  	<template v-slot:before> 
+  		<q-icon name="my_location" />
+  	</template>
+  	
+  	
+  	<template v-slot:append> 
+  		<q-btn round dense flat icon="search" />
+  	</template>
+  	</q-input>
+	</div>
+	
+	<div class="col text-white text-center">
+		<div class="text-h4 text-weight-light">
+			Bangalore
+		</div>
+	</div>
+  
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+  	return {
+  		search: '',
+  	}
+  }
+}
+</script>
+
+
+<style lang="scss">
+	.q-page{
+		background: linear-gradient(to bottom, #136a8a, #267871);
+	}
+</style>
